e6d694de54fd42d96c9090ad95ea108f3528939f
omarjebarah98 <134137810+omarjebarah98@users.noreply.github.com>
2023-10-19 23:34:51 +0300

    Delete ClusteredDataWarehouse/ClusteredDataWarehouse directory

--- ClusteredDataWarehouse/ClusteredDataWarehouse/src/main/java/com/DataWarehouse/ClusteredDataWarehouse/WarehouseRepository/WareHouseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.DataWarehouse.ClusteredDataWarehouse.WarehouseRepository;
-
-import com.DataWarehouse.ClusteredDataWarehouse.Model.Deals;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WareHouseRepository extends JpaRepository<Deals, Long> {
-}
